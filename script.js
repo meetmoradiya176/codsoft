4bd892ec28be1b47bab42fdbdfb31117ae57673f
Meet Moradiya <meetmoradiya176@gmail.com>
2025-08-03 21:49:49 +0530

    added projects section

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 function show_about_me (){
-    console.log("dasfsdf");
     document.getElementById("show_about_me_1").style.display = "flex";
     document.getElementById("show_about_me_2").style.display = "unset";
     document.getElementById("show_skill").style.display = "none";
+    document.getElementById("show_projects_1").style.display = "none";
+    document.getElementById("show_projects_2").style.display = "none";
 }
 
-function show_skill (){
-    console.log("dasfsdfadfsdgf");
+function show_skill(){
     document.getElementById("show_about_me_1").style.display = "none";
     document.getElementById("show_about_me_2").style.display = "none";
     document.getElementById("show_skill").style.display = "flex";
+    document.getElementById("show_projects_1").style.display = "none";
+    document.getElementById("show_projects_2").style.display = "none";
     document.getElementById("sub-node").style =  "animation: fadeInUp 1.5s ease-out";
+}
+
+function show_projects(){
+    document.getElementById("show_about_me_1").style.display = "none";
+    document.getElementById("show_about_me_2").style.display = "none";
+    document.getElementById("show_skill").style.display = "none";
+    document.getElementById("show_projects_1").style.display = "flex";
+    document.getElementById("show_projects_2").style.display = "flex";
 }
