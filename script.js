c60ba5e9298a3296c95be67ee2aeff62dbfd5a52
Meet Moradiya <meetmoradiya176@gmail.com>
2025-08-11 20:33:42 +0530

    created resume section

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function show_about_me (){
     document.getElementById("show_skill").style.display = "none";
     document.getElementById("show_projects_1").style.display = "none";
     document.getElementById("show_projects_2").style.display = "none";
+    document.getElementById("show_resume").style.display = "none";
 }
 
 function show_skill(){
@@ -12,6 +13,7 @@ function show_skill(){
     document.getElementById("show_skill").style.display = "flex";
     document.getElementById("show_projects_1").style.display = "none";
     document.getElementById("show_projects_2").style.display = "none";
+    document.getElementById("show_resume").style.display = "none";
     document.getElementById("sub-node").style =  "animation: fadeInUp 1.5s ease-out";
 }
 
@@ -21,4 +23,58 @@ function show_projects(){
     document.getElementById("show_skill").style.display = "none";
     document.getElementById("show_projects_1").style.display = "flex";
     document.getElementById("show_projects_2").style.display = "flex";
+    document.getElementById("show_resume").style.display = "none";
+}
+
+function show_resume(){
+    document.getElementById("show_about_me_1").style.display = "none";
+    document.getElementById("show_about_me_2").style.display = "none";
+    document.getElementById("show_skill").style.display = "none";
+    document.getElementById("show_projects_1").style.display = "none";
+    document.getElementById("show_projects_2").style.display = "none";
+    document.getElementById("show_resume").style.display = "flex";
+}
+
+
+
+
+
+function show_overview(){
+    document.getElementById("resume_basic_details_outer_div").style.display = "unset";
+    document.getElementById("experience_outer_div").style.display = "none";
+    document.getElementById("skills_outer_div").style.display = "none";
+    document.getElementById("education_outer_div").style.display = "none";
+    document.getElementById("contact_me_resume_outer_div").style.display = "none";
+}
+
+function show_experience(){
+    document.getElementById("resume_basic_details_outer_div").style.display = "none";
+    document.getElementById("experience_outer_div").style.display = "unset";
+    document.getElementById("skills_outer_div").style.display = "none";
+    document.getElementById("education_outer_div").style.display = "none";
+    document.getElementById("contact_me_resume_outer_div").style.display = "none";
+}
+
+function show_skills(){
+    document.getElementById("resume_basic_details_outer_div").style.display = "none";
+    document.getElementById("experience_outer_div").style.display = "none";
+    document.getElementById("skills_outer_div").style.display = "unset";
+    document.getElementById("education_outer_div").style.display = "none";
+    document.getElementById("contact_me_resume_outer_div").style.display = "none";
+}
+
+function show_education(){
+    document.getElementById("resume_basic_details_outer_div").style.display = "none";
+    document.getElementById("experience_outer_div").style.display = "none";
+    document.getElementById("skills_outer_div").style.display = "none";
+    document.getElementById("education_outer_div").style.display = "flex";
+    document.getElementById("contact_me_resume_outer_div").style.display = "none";
+}
+
+function show_contact_me(){
+    document.getElementById("resume_basic_details_outer_div").style.display = "none";
+    document.getElementById("experience_outer_div").style.display = "none";
+    document.getElementById("skills_outer_div").style.display = "none";
+    document.getElementById("education_outer_div").style.display = "none";
+    document.getElementById("contact_me_resume_outer_div").style.display = "flex";
 }
